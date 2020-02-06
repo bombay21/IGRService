db9be549efedec00cd7896f6a5ce6c67c23d5ddc
Kenechukwu Nnaji <P7849@fidelitybank.ng>
2020-02-06 06:51:01 +0100

    next commit

---
+++ NetCoreTut/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreTut.Models;
+
+namespace NetCoreTut.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Email;
+        }
+
+        public ViewResult Details()
+        {
+            Employee emp = _employeeRepository.GetEmployee(3);
+            return View(emp);
+        }
+    }
+}
---
+++ NetCoreTut/Models/MockEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetCoreTut.Models
+{
+    public class MockEmployee : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployee()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id=1, Name="Ken", Email="ken@gmail.com", Department="BA"},
+                new Employee() { Id = 2, Name = "Jake", Email = "Jake@gmail.com", Department = "E-Banking" },
+                new Employee() { Id = 3, Name = "Samuel", Email = "Sam@gmail.com", Department = "Digital Lab" }
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.Find(e => e.Id == Id);
+        }
+    }
+}
